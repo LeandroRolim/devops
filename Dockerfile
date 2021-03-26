5c754825288458f452545820619ddfa286828d2f
Leandro Rolim <leandro@viatechno.com.br>
2021-03-26 16:55:05 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:8-apache
+
+COPY index.php /var/www/html
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>DevOps</title>
+</head>
+<body>
+<?php
+    echo 'aula 3';
+?>
+</body>
+</html>
